7599d87964e92b254ff1b2ff1a925300c453b36a
yutasakurai <mw.y.sak@gmail.com>
2018-01-08 10:06:12 +0000

    timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,24 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
   
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
   
+  def feed_microposts
+    Micropost.where(user_id: self.following_ids + [self.id])
+  end
   
 end
 
